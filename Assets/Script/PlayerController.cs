1aa19a0fc459975b02563cd222646b8d0c2881de
Williamvlhg <136070530+Williamvlhg@users.noreply.github.com>
2023-07-28 14:53:21 +0200

    Init en 3D

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Vector2 direction;
-    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Rigidbody rb;
 
     public void Move(Vector2 moveTo)
     {
         direction = moveTo;
-        rb.velocity = new Vector2(direction.x, direction.y);
+        rb.velocity = new Vector3(direction.x, 0, direction.y);
     }
 }
+
